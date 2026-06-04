484d1c20d905293674d53d965e202d8a41d6932c
mohammadhasansojib <sajibhasan800080@gmail.com>
2026-06-04 19:02:25 +0600

    indentation, variable, comment basics

---
+++ syntax/syntax.py
@@ -0,0 +1,23 @@
+
+#* indentation
+
+# here space before the line print is indentation and it means a block of code.
+if 5 > 2: 
+    print("5 is greater than 2")
+
+
+# same block should be same number of space(at least 1)
+if 5 > 2:
+    print("line 1")
+    print("line 2")
+
+
+#* variable
+
+a = 50
+b = "hello world"
+
+print(a)
+
+
+# ===== this is comment ======
