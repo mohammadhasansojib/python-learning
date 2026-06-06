b361e01d3e474e4b4949e27a902fab413bfefd52
mohammadhasansojib <sajibhasan800080@gmail.com>
2026-06-06 21:23:57 +0600

    learn variable basic type casting, case-sensitive concert, naming

---
+++ variable/variable.py
@@ -0,0 +1,36 @@
+
+#* casting
+# If you want to specify the data type of a variable, this can be done with casting.
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#* Case-Sensitive
+# Variable names are case-sensitive.
+
+a = 5
+A = 30
+# 'a' and 'A' are different
+
+#* variable Naming
+
+# legal names
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+# illegal names
+"""
+2myvar = "John"
+my-var = "John"
+my var = "John"
+"""
